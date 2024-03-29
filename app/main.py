d2b84bc21017ac2460d5f7a138640f5eb12f4d90
Gwenael Weinstein <gwenael.weinstein@gmail.com>
2024-03-29 11:22:36 +0100

    feat: explain project in app

--- app/main.py
+++ app/main.py
@@ -2,3 +2,10 @@ import pandas as pd
 import streamlit as st
 
 st.title("World Happiness on Earth")
+
+st.write("An analysis of well-being on Earth based on data collected by the [World Happiness Report](https://worldhappiness.report/), whose survey aims to estimate the level of happiness by country based on socio-economic measures around health, education, corruption, economy, life expectancy, etc.")
+
+st.write("This work has been conducted as part of the [Data Analyst training program at DataScientest](https://datascientest.com/formation-data-analyst) in partnership with [Mines ParisTech PSL](https://executive-education.minesparis.psl.eu/).")
+
+st.image('assets/whr-cover.jpg', use_column_width=True)
+st.caption('Credits: image by [Phạm Quốc Nguyên](https://pixabay.com/fr/users/sanshiro-5833092)')
