761939eb359b319a69089d71e9bb6020bdaf15f7
Gwenael Weinstein <gwenael.weinstein@gmail.com>
2024-03-29 16:51:17 +0100

    feat: main dataset with definitions

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import streamlit as st
 
+WHR_URL = 'https://happiness-report.s3.amazonaws.com/2023/DataForTable2.1WHR2023.xls'
+whr = pd.read_excel(WHR_URL)
+whr['year'] = whr['year'].astype(str)
+
 st.title("World Happiness on Earth")
 
-tabs = ["Home",
-        "Introduction"]
+pages = ["Home",
+        "Introduction",
+        "Data exploration"]
 
-tab = st.sidebar.radio("", options = tabs)
+page = st.sidebar.radio("", options = pages)
 
-if tab != tabs[0]:
-    st.header(tab)
+if page != pages[0]:
+    st.header(page)
 
-if tab == tabs[0]:
+if page == pages[0]:
     st.write("An analysis of well-being on Earth based on data collected by the [World Happiness Report](https://worldhappiness.report/), whose survey aims to estimate the level of happiness by country based on socio-economic measures around health, education, corruption, economy, life expectancy, etc.")
 
     st.write("This work has been conducted as part of the [Data Analyst training program at DataScientest](https://datascientest.com/formation-data-analyst) in partnership with [Mines ParisTech PSL](https://executive-education.minesparis.psl.eu/).")
@@ -19,10 +24,10 @@ if tab == tabs[0]:
     st.image('assets/whr-cover.jpg', use_column_width=True)
     st.caption('Credits: image by [Phạm Quốc Nguyên](https://pixabay.com/fr/users/sanshiro-5833092)')
 
-if tab == tabs[1]:
+if page == pages[1]:
     st.write("The **World Happiness Report** is a publication of the [Sustainable Development Solutions Network](https://www.unsdsn.org/) for the United Nations, and powered by [Gallup World Poll](https://www.gallup.com/178667/gallup-world-poll-work.aspx) data. Its goal is to give more importance to happiness and well-being as criteria for assessing government policies.")
 
-    st.write("**Gallup World Poll** ratings are based on the *Cantril Ladder*, where respondents are asked to evaluate their own life by giving a grade between 0 (the worst life they can think of) and 10 (the best one). The **World Happiness Report** adds 6 socio-economic measures: *GDP per capita*, *Healthy life expectancy at birth*, *Social support*, *Generosity*, *Freedom to make life choices* and *Corruption*.")
+    st.write("**Gallup World Poll** ratings are based on the *Cantril Ladder*, where respondents are asked to evaluate their own life by giving a grade between 0 (the worst life they can think of) and 10 (the best one). The **World Happiness Report** adds 6 socio-economic measures and 2 daily feelings.")
 
     st.write("A report is published every year since 2012, with data from 2005 to the previous year of the publication.")
 
@@ -32,3 +37,74 @@ if tab == tabs[1]:
 
     st.caption("The current project and app have been done with data from the [2023 report](https://worldhappiness.report/ed/2023/). We may be able to test our process with data from 2024 at the end of this work.")
 
+if page == pages[2]:
+    st.subheader("Show data")
+    st.dataframe(whr)
+
+    st.write('''
+        - The dataset is structured around 2 categorical variables (objects): :grey[*Country name*] and :grey[*year*]. Each record corresponds to the result of the survey for a given year and country: we call them *indexers*.
+        - The *target* is represented by the variable :grey[*Life Ladder*] (float).
+        - All other variables (floats) are *features*.
+    ''')
+
+    st.write("")
+    
+    with st.expander("Show variables definitions"):
+        st.write('''
+            :grey[*Country name*]  
+            Country described by the data of the record.
+        ''')
+
+        st.write('''
+            :grey[*year*]  
+            Year of data recording for the row.
+        ''')
+
+        st.write('''
+            :grey[*Life Ladder*]  
+            Happiness level of a country according to the Cantril ladder, a scale between 0 and 10.
+        ''')
+
+        st.write('''
+            :grey[*Log GDP per capita*]  
+            Gross Domestic Product (GDP) per capita. This column provides information about the size and performance of the economy.
+        ''')
+
+        st.write('''
+            :grey[*Social support*]  
+            Ratio of respondents who answered *"YES"* to the question: *"If you encounter difficulties, do you have relatives or friends you can count on to help you?"*
+        ''')
+
+        st.write('''
+            :grey[*Healthy life expectancy at birth*]  
+            Measures the physical and mental health of a country's population, based on data provided by the World Health Organization (WHO).
+        ''')
+
+        st.write('''
+            :grey[*Freedom to make life choices*]  
+            Ratio of respondents who answered *"YES"* to the question: *"Are you satisfied or dissatisfied with your freedom of choice/action?"*
+        ''')
+
+        st.write('''
+            :grey[*Generosity*]  
+            Ratio of respondents who answered *"YES"* to the question: *"Did you donate money to a charity last month?"*
+        ''')
+
+        st.write('''
+            :grey[*Perceptions of corruption*]  
+            Perception by the population of the level of corruption in their country (at both political - institutions - and economic - businesses - levels).
+        ''')
+
+        st.write('''
+            :grey[*Positive affect*]  
+            Average of positive or negative responses given in relation to three emotions: laughter, pleasure, and interest.
+        ''')
+
+        st.write('''
+            :grey[*Negative affect*]  
+            Average of positive or negative responses given in relation to three emotions: concern, sadness, and anger.
+        ''')
+    
+    st.write("")
+
+    st.caption("Note that the *year* variable has been cast to an object to prevent Streamlit from displaying it as a float with thousand separators, knowing that it is not used for time series in our context.")
