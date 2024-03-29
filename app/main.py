3b292d494bb99eb9308fe1ee5f422f782dd4fcef
Gwenael Weinstein <gwenael.weinstein@gmail.com>
2024-03-29 15:18:22 +0100

    feat: introduction

--- app/main.py
+++ app/main.py
@@ -3,9 +3,32 @@ import streamlit as st
 
 st.title("World Happiness on Earth")
 
-st.write("An analysis of well-being on Earth based on data collected by the [World Happiness Report](https://worldhappiness.report/), whose survey aims to estimate the level of happiness by country based on socio-economic measures around health, education, corruption, economy, life expectancy, etc.")
+tabs = ["Home",
+        "Introduction"]
 
-st.write("This work has been conducted as part of the [Data Analyst training program at DataScientest](https://datascientest.com/formation-data-analyst) in partnership with [Mines ParisTech PSL](https://executive-education.minesparis.psl.eu/).")
+tab = st.sidebar.radio("", options = tabs)
 
-st.image('assets/whr-cover.jpg', use_column_width=True)
-st.caption('Credits: image by [Phạm Quốc Nguyên](https://pixabay.com/fr/users/sanshiro-5833092)')
+if tab != tabs[0]:
+    st.header(tab)
+
+if tab == tabs[0]:
+    st.write("An analysis of well-being on Earth based on data collected by the [World Happiness Report](https://worldhappiness.report/), whose survey aims to estimate the level of happiness by country based on socio-economic measures around health, education, corruption, economy, life expectancy, etc.")
+
+    st.write("This work has been conducted as part of the [Data Analyst training program at DataScientest](https://datascientest.com/formation-data-analyst) in partnership with [Mines ParisTech PSL](https://executive-education.minesparis.psl.eu/).")
+
+    st.image('assets/whr-cover.jpg', use_column_width=True)
+    st.caption('Credits: image by [Phạm Quốc Nguyên](https://pixabay.com/fr/users/sanshiro-5833092)')
+
+if tab == tabs[1]:
+    st.write("The **World Happiness Report** is a publication of the [Sustainable Development Solutions Network](https://www.unsdsn.org/) for the United Nations, and powered by [Gallup World Poll](https://www.gallup.com/178667/gallup-world-poll-work.aspx) data. Its goal is to give more importance to happiness and well-being as criteria for assessing government policies.")
+
+    st.write("**Gallup World Poll** ratings are based on the *Cantril Ladder*, where respondents are asked to evaluate their own life by giving a grade between 0 (the worst life they can think of) and 10 (the best one). The **World Happiness Report** adds 6 socio-economic measures: *GDP per capita*, *Healthy life expectancy at birth*, *Social support*, *Generosity*, *Freedom to make life choices* and *Corruption*.")
+
+    st.write("A report is published every year since 2012, with data from 2005 to the previous year of the publication.")
+
+    st.write("> *With this project, we want to present this data using interactive visualizations and determine combinations of factors that explain why some countries are better ranked than others.*")
+
+    st.write("")
+
+    st.caption("The current project and app have been done with data from the [2023 report](https://worldhappiness.report/ed/2023/). We may be able to test our process with data from 2024 at the end of this work.")
+
